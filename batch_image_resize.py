a066803ac9974d96ac80cd2ac6faa48a3e1243a1
Hritik Zurange <62016342+zhritik@users.noreply.github.com>
2024-02-25 19:28:40 -0600

    Update batch_image_resize.py

--- batch_image_resize.py
+++ batch_image_resize.py
@@ -8,26 +8,25 @@ Created on Wed Dec  6 17:51:50 2023
 from PIL import Image
 import os
 
-# Set the path to your image folder
+#setting path to image folders
 input_folder = "P:\Dataset\Merged_Dataset"
 output_folder = "P:\Dataset\Merged_Dataset_Processed"
 
-# Set the target size
+#target size of images
 target_size = (48, 48)
 
-# Ensure the output folder exists
 os.makedirs(output_folder, exist_ok=True)
 
-# Loop through each image in the input folder
+#loop and resize
 for filename in os.listdir(input_folder):
-    # Load the image
+    #loading OG image
     input_path = os.path.join(input_folder, filename)
     img = Image.open(input_path)
 
-    # Resize the image
+    #resizing the image
     resized_img = img.resize(target_size)
 
-    # Save the resized image to the output folder
+    #store resized image
     output_path = os.path.join(output_folder, filename)
     resized_img.save(output_path)
 
